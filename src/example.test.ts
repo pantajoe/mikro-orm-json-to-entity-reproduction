d0b307c778df994d578fdb0ac6f6bd32680c68e5
Joe Pantazidis <joe.pantazidis@outlook.de>
2025-03-25 11:11:08 +0100

    test: add repro

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,22 +1,33 @@
-import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/sqlite';
+import { Entity, Property, OneToMany, Collection, ManyToOne, Ref, MikroORM, BaseEntity as OrmBase, PrimaryKey, Opt, wrap, helper } from '@mikro-orm/sqlite'
 
 @Entity()
-class User {
+abstract class BaseEntity extends OrmBase {
+  @PrimaryKey({ autoincrement: true })
+    id: number
 
-  @PrimaryKey()
-  id!: number;
+  @Property({ type: 'date', onUpdate: () => new Date() })
+    updatedAt: Date & Opt = new Date()
 
+  @Property({ type: 'date', onCreate: () => new Date() })
+    createdAt: Date & Opt = new Date()
+}
+
+@Entity()
+class Author extends BaseEntity {
   @Property()
-  name: string;
+    name: string
 
-  @Property({ unique: true })
-  email: string;
+  @OneToMany(() => Book, book => book.author)
+    books = new Collection<Book>(this)
+}
 
-  constructor(name: string, email: string) {
-    this.name = name;
-    this.email = email;
-  }
+@Entity()
+class Book extends BaseEntity {
+  @Property()
+    title: string
 
+  @ManyToOne(() => Author, { ref: true })
+    author: Ref<Author>
 }
 
 let orm: MikroORM;
@@ -26,6 +37,7 @@ beforeAll(async () => {
     dbName: ':memory:',
     entities: [User],
     debug: ['query', 'query-params'],
+    persistOnCreate: true,
     allowGlobalContext: true, // only for testing
   });
   await orm.schema.refreshDatabase();
@@ -35,17 +47,32 @@ afterAll(async () => {
   await orm.close(true);
 });
 
-test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
-  await orm.em.flush();
-  orm.em.clear();
+test('can "hydrate" an entity from JSON without querying the DB', async () => {
+  const author = orm.em.create(Author, { name: 'Jon Snow' })
+  orm.em.create(Book, { title: 'Book of the North', author })
+  await orm.em.flush()
+  const pojo = wrap(author).toJSON() // Serialize to JSON including the books Collection
+  orm.em.clear() // Emulate new Request Context
 
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
-  await orm.em.flush();
+  const entity = orm.em.create(Author, pojo, { managed: false, persist: false }) // Create Entity that can be used, but treat as if it were loaded via a findOne query without doing a trip to the DB
+  expect(wrap(entity).toPOJO()).toEqual({
+    id: 1,
+    name: 'Jon Snow',
+    createdAt: expect.any(Date),
+    updatedAt: expect.any(Date),
+    books: [
+      {
+        id: 1,
+        title: 'Book of the North',
+        createdAt: expect.any(Date),
+        updatedAt: expect.any(Date),
+        author: expect.objectContaining({ id: 1 })
+      }
+    ]
+  })
 
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+  // Make sure that the changesert is empty
+  orm.em.getUnitOfWork().computeChangeSets()
+  expect(orm.em.getUnitOfWork().getChangeSets().length).toBe(0)
+  await expect(orm.em.flush()).resolves.not.toThrow()
 });
